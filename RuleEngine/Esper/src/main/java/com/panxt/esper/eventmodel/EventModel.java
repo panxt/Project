30be1c40a7c527d75d96c02213cf4712d2a47268
panxt <15639727428@163.com>
2020-06-11 17:32:26 +0800

    well done

---
+++ RuleEngine/Esper/src/main/java/com/panxt/esper/eventmodel/EventModel.java
@@ -0,0 +1,13 @@
+package com.panxt.esper.eventmodel;
+
+/**
+ * @author panxt
+ */
+public class EventModel {
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
--- spring/src/main/java/com/panxt/spring/MybatisDemoApplication.java
+++ spring/src/main/java/com/panxt/spring/MybatisDemoApplication.java
@@ -3,12 +3,15 @@ package com.panxt.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author panxt
+ */
 @SpringBootApplication
 public class MybatisDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MybatisDemoApplication.class, args);
-		System.out.println("hello spring");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisDemoApplication.class, args);
+        System.out.println("hello spring");
+    }
 
 }
